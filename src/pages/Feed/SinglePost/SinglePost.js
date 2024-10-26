6d07f0f03b26940727422be684a94b165aa8097b
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-10-26 13:09:12 +0530

    Added url to fetching single post

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Image from '../../../components/Image/Image';
 import './SinglePost.css';
+import post from '../../../components/Feed/Post/Post';
+import image from '../../../components/Image/Image';
 
 class SinglePost extends Component {
   state = {
@@ -14,7 +16,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    fetch('http://localhost:8080/feeds/posts/' + postId)
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
@@ -22,11 +24,14 @@ class SinglePost extends Component {
         return res.json();
       })
       .then(resData => {
+        console.log('http://localhost:8080' + resData.post.imageUrl);
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
+          image : 'http://localhost:8080/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
+          content: resData.post.content,
+          
         });
       })
       .catch(err => {
